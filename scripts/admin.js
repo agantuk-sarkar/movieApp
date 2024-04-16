c15c86d90fe9405e71937e05e8eef0d7e89fa89f
Agantuk Sarkar <agantuksarkar124@gmail.com>
2024-04-17 01:39:42 +0530

    Added new movie images

--- scripts/admin.js
+++ scripts/admin.js
@@ -2,7 +2,7 @@
 const admin_form = document.querySelector(".movie-form");
 
 // array to store form input values
-const movie_array = JSON.parse(localStorage.getItem("movieShows")) || [];
+const movie_array = JSON.parse(localStorage.getItem("movieShows"));
 
 // using constructor to create objects which will store input values
 class Movie {
@@ -25,11 +25,12 @@ admin_form.addEventListener("submit", function (event) {
   const imdb_rating = document.getElementById("imdbRating").value;
 
   // using new keyword to create new object everytime
-  const movie = new Movie(movie_name,images,release_date,imdb_rating);
+  const movie = new Movie(movie_name, images, release_date, imdb_rating);
 
   // pushing the newly created object into the movie array
   movie_array.push(movie);
 
   // storing the new array of objects into localStorage
   localStorage.setItem("movieShows", JSON.stringify(movie_array));
+  console.log(movie_array);
 });
